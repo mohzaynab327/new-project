8768408af28d1214d78a2798271f95aaadbc5123
zaynab <mohzaynab327@gmail.com>
2024-12-23 13:11:53 +0300

    my second repository

--- BACKEND/PROJECT/PROJECTapp/admin.py
+++ BACKEND/PROJECT/PROJECTapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .model import Product, Supplier ,inventory
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Supplier)
+admin.site.register(Inventory)
